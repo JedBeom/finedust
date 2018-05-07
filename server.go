4685331e16b61654554edb3582f61ff6239cbfe4
Junhwan Beom <solkblte@icloud.com>
2018-05-07 17:35:44 +0900

    test.xml 파일을 잘 읽어오는 것을 확인.

--- server.go
+++ server.go
@@ -3,32 +3,11 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
-var xmlFile = []byte(`
-<item>
-    <dataTime>2018-05-03 21:00</dataTime>
-    <mangName>도시대기</mangName>
-    <so2Value>0.003</so2Value>
-    <coValue>0.4</coValue>
-    <o3Value>0.054</o3Value>
-    <no2Value>0.005</no2Value>
-    <pm10Value>31</pm10Value>
-    <pm10Value24>-</pm10Value24>
-    <pm25Value>16</pm25Value>
-    <pm25Value24>-</pm25Value24>
-    <khaiValue>-</khaiValue>
-    <khaiGrade/>
-    <so2Grade>1</so2Grade>
-    <coGrade>1</coGrade>
-    <o3Grade>2</o3Grade>
-    <no2Grade>1</no2Grade>
-    <pm10Grade/>
-    <pm25Grade/>
-    <pm10Grade1h>2</pm10Grade1h>
-    <pm25Grade1h>2</pm25Grade1h>
-</item>`)
-
+// 본격적인 데이터가 들어가는 구조체
 type item struct {
 	Time      string `xml:"dataTime"`
 	Pm10Value int    `xml:"pm10Value"`
@@ -50,22 +29,20 @@ type response struct {
 }
 
 func main() {
-	var fine item
+	var fine response
 
-	/*
-		xp, err := os.Open("easiler.xml")
-		if err != nil {
-			panic(err)
-		}
-		defer xp.Close()
+	xp, err := os.Open("test.xml")
+	if err != nil {
+		panic(err)
+	}
+	defer xp.Close()
 
-		data, err := ioutil.ReadAll(xp)
-		if err != nil {
-			panic(err)
-		}
-	*/
+	data, err := ioutil.ReadAll(xp)
+	if err != nil {
+		panic(err)
+	}
 
-	err := xml.Unmarshal(xmlFile, &fine)
+	err = xml.Unmarshal(data, &fine)
 	if err != nil {
 		panic(err)
 	}
