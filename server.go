3445bff58102b6d086b615c266e6414266a37df5
Junhwan Beom <solkblte@icloud.com>
2018-06-03 15:14:30 +0900

    Deleted script.js, add supporting of long HangulRates

--- server.go
+++ server.go
@@ -16,15 +16,16 @@ import (
 
 // 본격적인 데이터가 들어가는 구조체
 type item struct {
-	Time        string `xml:"dataTime"`
-	Pm10Value   int    `xml:"pm10Value"`
-	Pm25Value   int    `xml:"pm25Value"`
-	Pm10Rate    int
-	Pm25Rate    int
-	MixedRate   int
-	HangulRate  string
-	Background  string
-	stationName string
+	Time             string `xml:"dataTime"`
+	Pm10Value        int    `xml:"pm10Value"`
+	Pm25Value        int    `xml:"pm25Value"`
+	Pm10Rate         int
+	Pm25Rate         int
+	MixedRate        int
+	HangulRate       string
+	IsHangulRateLong bool
+	Background       string
+	stationName      string
 }
 
 /*
@@ -41,7 +42,7 @@ type response struct {
 	Body    body     `xml:"body"`
 }
 
-func thisTime(stationName string) response {
+func getDataOfNow(stationName string) response {
 	var full response
 	full = response{Body: body{Item: item{stationName: stationName}}}
 
@@ -62,17 +63,18 @@ func thisTime(stationName string) response {
 	if err != nil {
 		log.Printf("I've got an error while Unmarshal XML file. stationName: %v", stationName)
 		if stationName != "장천동" {
-			full = thisTime("장천동")
+			full = getDataOfNow("장천동")
 		} else {
-			full = thisTime("연향동")
+			full = getDataOfNow("연향동")
 		}
 	}
 	return full
 }
 
+//Gives you a rate of fine dust
 func rater(Item item) item {
 	// 미세먼지 등급을 메기기(1-8)
-	// 높을수록 공기의 상태가 좋음
+	// 등급이 낮을 수록 공기의 상태가 좋음
 	pm10 := Item.Pm10Value
 	switch {
 	case pm10 <= 15:
@@ -93,7 +95,7 @@ func rater(Item item) item {
 		Item.Pm10Rate = 8
 	}
 	// 초미세먼지 등급을 메김(1-8)
-	// 등급이 높을수록 공기의 상태가 좋음
+	// 등급이 낮을수록 공기의 상태가 좋음
 	pm25 := Item.Pm25Value
 	switch {
 	case pm25 <= 8:
@@ -116,8 +118,9 @@ func rater(Item item) item {
 	return Item
 }
 
+//Main Handler.
 func sender(w http.ResponseWriter, r *http.Request) {
-	full := thisTime("연향동")
+	full := getDataOfNow("연향동")
 	Item := full.Body.Item
 	Item = rater(Item)
 	Item = MixingRatesAndGiveAHangulRate(Item)
@@ -147,6 +150,7 @@ func open(url string) error {
 	return exec.Command(cmd, args...).Start()
 }
 
+//Show The Highest Rate of them and gives you background-color
 func MixingRatesAndGiveAHangulRate(Item item) item {
 	if Item.Pm10Rate >= Item.Pm25Rate {
 		Item.MixedRate = Item.Pm10Rate
@@ -157,6 +161,7 @@ func MixingRatesAndGiveAHangulRate(Item item) item {
 	}
 	var rate string
 	var color string
+	var isitlong bool
 
 	switch Item.MixedRate {
 	case 1:
@@ -177,38 +182,43 @@ func MixingRatesAndGiveAHangulRate(Item item) item {
 	case 6:
 		rate = "상당히 나쁨"
 		color = "#c94e2c"
+		isitlong = true
 	case 7:
 		rate = "매우 나쁨"
 		color = "#b83134"
+		isitlong = true
 	case 8:
 		rate = "최악"
 		color = "#262626"
 	}
 	Item.HangulRate = rate
 	Item.Background = color
+	Item.IsHangulRateLong = isitlong
 	return Item
 }
 
 func main() {
+
+	//Server Part
 	port := ":8080"
 	fmt.Println("Server Started at port", port)
 	server := http.Server{
 		Addr: port,
 	}
+	//Serve static files like CSS
 	files := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", sender)
+
+	//Open Chrome on this computer
 	go open("http://localhost:8080")
 	time.Sleep(1 * time.Second)
+	//Make Full-Size Screen
 	go robotgo.KeyTap("f11")
+
+	//Start Server
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Error on ListenAndServe()")
 	}
-	/*
-		full := thisTime()
-		Item := full.Body.Item
-		Item = rater(Item)
-		fmt.Println(Item)
-	*/
 }
